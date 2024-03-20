933407fb5fe934d0d350dffdcf178b2ae92d0b6d
P4rley <adwihersa@gmail.com>
2024-03-20 09:19:29 +0700

    Initial Commit

---
+++ twitter-clone/twitter_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitter_cloneApp.swift
+//  twitter-clone
+//
+//  Created by Adwitya Hersa on 20/03/24.
+//
+
+import SwiftUI
+
+@main
+struct twitter_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
